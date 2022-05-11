e6c47c902708fa7cccc02f6187e36be50366354b
maialen24 <maiaru24@gmail.com>
2022-05-11 16:37:19 +0200

    multilingüe

--- app/src/main/java/com/example/kubuk/login/LoginActivity.java
+++ app/src/main/java/com/example/kubuk/login/LoginActivity.java
@@ -2,11 +2,15 @@ package com.example.kubuk.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -18,6 +22,7 @@ import com.android.volley.toolbox.Volley;
 import com.example.kubuk.Main.MenuMain;
 import com.example.kubuk.R;
 
+import java.util.Locale;
 import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity implements Response.Listener<String>, Response.ErrorListener {
@@ -25,12 +30,21 @@ public class LoginActivity extends AppCompatActivity implements Response.Listene
     EditText textEmail, textPasswd;
     Button loginBoton, registerBoton;
     RequestQueue request;
+    private String language;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState!= null) {
+            language =savedInstanceState.getString("language");
+        }
         setContentView(R.layout.activity_login);
+
+        TextView eu=(TextView) findViewById(R.id.euskera);
+        TextView en=(TextView) findViewById(R.id.ingles);
+        TextView es=(TextView) findViewById(R.id.español);
 
         textEmail = findViewById(R.id.textEmailReg);
         textPasswd = findViewById(R.id.textPasswordReg1);
@@ -51,6 +65,44 @@ public class LoginActivity extends AppCompatActivity implements Response.Listene
 
         registerBoton.setOnClickListener(view -> {
             registrarse();
+        });
+
+        //euskera button (textView) clicked, change language
+        eu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("info-paso","Euskera");
+                language="eu";
+                changeLanguage();
+                finish();
+                startActivity(getIntent());
+            }
+        });
+
+        // english button (textview) clicked change language
+        en.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("info-paso","Ingelesa");
+                language="en";
+                changeLanguage();
+                finish();
+                startActivity(getIntent());
+                //onStart();
+            }
+        });
+
+        //spanish button (textview) clicked change language
+        es.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("info-paso","Castellano");
+                language="es";
+                changeLanguage();
+                finish();
+                startActivity(getIntent());
+                // onStart();
+            }
         });
 
     }
@@ -118,5 +170,65 @@ public class LoginActivity extends AppCompatActivity implements Response.Listene
         finish();
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    //metodo laguntzaile to change language
+    private void changeLanguage(){
+
+        Locale nuevaloc = new Locale(language);
+        Locale.setDefault(nuevaloc);
+        Configuration configuration = getBaseContext().getResources().getConfiguration();
+        configuration.setLocale(nuevaloc);
+        configuration.setLayoutDirection(nuevaloc);
+
+        Context context = getBaseContext().createConfigurationContext(configuration);
+        getBaseContext().getResources().updateConfiguration(configuration, context.getResources().getDisplayMetrics());
+
+    }
+
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("language",language );
+    }
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        language = savedInstanceState.getString("language");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("CONTROL","login ON PAUSE");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("CONTROL"," login ON START");
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("CONTROL","login ON RESUME");
+
+
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("CONTROL","login ON STOP");
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("CONTROL","login ON DESTROY");
+
     }
 }
